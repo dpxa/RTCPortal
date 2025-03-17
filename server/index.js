4e6d622de1dd2930bdd29da84ac361138b26d164
dpxa <Andrewmac2022@gmail.com>
2025-03-17 12:42:46 -0700

    attempt to fix occasional app shutdowns

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 app.get("/test", (req, res) => {
+  console.log(`[${getTimestampPST()}] Ping`);
   res.status(200).send(`
     <h1>RTC Portal</h1>
     <p>Server is running.</p>
@@ -33,9 +34,6 @@ io.on("connection", (socket) => {
 
   // listen for an "offer" event from a client
   socket.on("offer", (payload) => {
-    console.log(
-      `[${getTimestampPST()}] Received offer from ${socket.id} to ${payload.target}`
-    );
     // relay it to payload.target
     io.to(payload.target).emit("offer", {
       sdp: payload.sdp,
@@ -45,11 +43,6 @@ io.on("connection", (socket) => {
 
   // listen for an "answer" event from a client
   socket.on("answer", (payload) => {
-    console.log(
-      `[${getTimestampPST()}] Received answer from ${socket.id} to ${
-        payload.target
-      }`
-    );
     // relay it to payload.target
     io.to(payload.target).emit("answer", {
       sdp: payload.sdp,
@@ -59,20 +52,11 @@ io.on("connection", (socket) => {
 
   // listen for ICE "candidate" events
   socket.on("candidate", (payload) => {
-    console.log(
-      `[${getTimestampPST()}] Received candidate from ${socket.id} to ${
-        payload.target
-      }`
-    );
     // relay it to payload.target to add to their RTCPeerConnection
     io.to(payload.target).emit("candidate", {
       candidate: payload.candidate,
       from: socket.id,
     });
-  });
-
-  socket.on("disconnect", () => {
-    console.log(`[${getTimestampPST()}] Socket disconnected: ${socket.id}`);
   });
 });
 
